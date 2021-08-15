522e4efac8e57c27712b9f27df63a982f621c321
Özgürcan Çalışkan <61111891+ozgurcanc@users.noreply.github.com>
2021-08-15 17:52:33 +0300

    Add random class

---
+++ Project Horizon/HorizonEngine/Random.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace HorizonEngine
+{
+    public static class Random
+    {
+        private static System.Random _random;
+
+        static Random()
+        {
+            _random = new System.Random();
+        }
+
+        public static int Range(int minInclusive, int maxExclusive)
+        {
+            return _random.Next(minInclusive, maxExclusive);
+        }
+
+        public static float Range(float minInclusive, float maxExclusive)
+        {
+            return (float)_random.NextDouble() * (maxExclusive - minInclusive) + minInclusive;
+        }
+    }
+}
