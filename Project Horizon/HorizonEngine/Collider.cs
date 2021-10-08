060685b1010e60f987a1b1026100e86777043adf
Özgürcan Çalışkan <61111891+ozgurcanc@users.noreply.github.com>
2021-10-08 19:53:46 +0300

    Add physicsMaterial to collider

--- Project Horizon/HorizonEngine/Collider.cs
+++ Project Horizon/HorizonEngine/Collider.cs
@@ -14,12 +14,28 @@ namespace HorizonEngine
     public abstract class Collider : Component
     {
         [JsonIgnore]
+        private PhysicsMaterial _physicsMaterial;
+        private uint _assetID;
+        [JsonIgnore]
         private TransformMatrix _transform;
         [JsonIgnore]
         private Rigidbody _attachedRigidbody;
         [JsonIgnore]
         private AABB _aabb;
         private bool _isTrigger = false;
+
+        public PhysicsMaterial physicsMaterial
+        {
+            get
+            {
+                return _physicsMaterial;
+            }
+            set
+            {
+                _physicsMaterial = value;
+                _assetID = value == null ? 0 : _physicsMaterial.assetID;
+            }
+        }
 
         public bool isTrigger
         {
@@ -81,6 +97,11 @@ namespace HorizonEngine
             Scene.DisableComponent(this);
         }
 
+        public override void OnLoad()
+        {
+            physicsMaterial = Assets.GetPhysicsMaterial(_assetID);
+        }
+
         public override void OnInspectorGUI()
         {
             string id = this.GetType().Name + componetID.ToString();
@@ -92,6 +113,39 @@ namespace HorizonEngine
             {
                 Undo.RegisterAction(this, this.isTrigger, isTrigger, nameof(Collider.isTrigger));
                 this.isTrigger = isTrigger;
+            }
+
+            string physicsMaterialName = _physicsMaterial == null ? "None" : _physicsMaterial.name;
+            ImGui.Text("Physics Material");
+            ImGui.SameLine();
+            ImGui.Text(physicsMaterialName);
+            //ImGui.SameLine();
+            if (ImGui.Button("Select Physics Material"))
+            {
+                ImGui.OpenPopup("select_physics_material");
+                SearchBar.Clear();
+            }
+
+            if (ImGui.BeginPopup("select_physics_material"))
+            {
+                SearchBar.Draw();
+
+                if (ImGui.Selectable("None"))
+                {
+                    Undo.RegisterAction(this, this.physicsMaterial, null, nameof(Collider.physicsMaterial));
+                    this.physicsMaterial = null;
+                }
+
+                foreach (PhysicsMaterial physicsMaterial in Assets.physicsMaterials)
+                {
+                    if (SearchBar.PassFilter(physicsMaterial.name) && ImGui.Selectable(physicsMaterial.name))
+                    {
+                        Undo.RegisterAction(this, this.physicsMaterial, physicsMaterial, nameof(Collider.physicsMaterial));
+                        this.physicsMaterial = physicsMaterial;
+                    }
+                }
+
+                ImGui.EndPopup();
             }
         }
 
